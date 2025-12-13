f151cc2ddebed913931d2fe183d4f567853443f6
Hossein Maklavani <hos.mohamadi@gmail.com>
2025-12-13 15:43:13 +0330

    Upgraded the packages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [],
-  },
+	reactStrictMode: true,
+	images: {
+		domains: []
+	}
 }
 
 module.exports = nextConfig
-
